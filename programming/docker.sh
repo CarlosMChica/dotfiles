ca1b13f8aa96bed6f6ce7ac63d0c24a486ce94ff
Christian Panadero <panavtec@gmail.com>
2018-07-22 16:38:04 +0100

    Add docker functions

--- programming/docker.sh
+++ programming/docker.sh
@@ -44,3 +44,13 @@ dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/[
 
 # Bash into running container
 dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+
+dockerCleanUpContainers() {
+  docker ps -aq | xargs docker stop
+  docker ps -aq | xargs docker rm
+}
+
+dockerPrune() {
+  docker system prune
+  docker rmi "$(docker images -a -q)"
+}
