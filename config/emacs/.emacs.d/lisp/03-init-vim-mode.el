f2045383dfec0ba66306f7acbba2f4052121b1e3
Carlos Morera de la Chica <carlos@codurance.com>
2018-07-31 18:56:30 +0100

    Add my emacs navigation

--- config/emacs/.emacs.d/lisp/03-init-vim-mode.el
+++ config/emacs/.emacs.d/lisp/03-init-vim-mode.el
@@ -37,6 +37,11 @@
   (define-key evil-motion-state-map (kbd "C-w h") 'evil-window-left)
   (define-key evil-motion-state-map (kbd "C-w l") 'evil-window-right)
 
+  (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+
   ;; Buffer navigation
   (define-key evil-motion-state-map (kbd "[ b") 'evil-prev-buffer)
   (define-key evil-motion-state-map (kbd "] b") 'evil-next-buffer)
