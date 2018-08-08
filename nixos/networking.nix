259e94843345c303f4317ad87a91742183cafcee
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-08 22:07:20 +0100

    Open chromecast ports in nixos

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -16,7 +16,8 @@ in
   # networking.firewall.allowedTCPPorts = [ 8080 80 3000 ];
   # networking.firewall.allowedUDPPorts = [ ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+
+  networking.firewall.allowedTCPPorts = [ 5556 5558 ];
 
   environment.systemPackages = with pkgs; [
     iw
