b43fbd55c13b86613502e419c47644450396013f
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-08 17:51:39 +0100

    Add Intellij ultimate to nixos

--- nixos/java.nix
+++ nixos/java.nix
@@ -2,6 +2,10 @@
 
 {
   environment.systemPackages = with pkgs; [
-    openjdk openjdk10 maven gradle jetbrains.idea-community
+    openjdk
+    openjdk10
+    maven
+    gradle
+    jetbrains.idea-ultimate
   ];
 }
