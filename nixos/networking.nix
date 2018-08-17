cc1cb7c783cb9ffbfe61988b31bcc05e837f5e3e
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-17 19:43:11 +0100

    Simplify networking

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -4,15 +4,7 @@
 
   networking = {
     networkmanager.enable = true;
-    wireguard.interfaces = wireguardInterfaces;
   };
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 8080 80 3000 ];
-  # networking.firewall.allowedUDPPorts = [ ];
-  # Or disable the firewall altogether.
-
-  networking.firewall.allowedTCPPorts = [ 5556 5558 ];
 
   environment.systemPackages = with pkgs; [
     iw
