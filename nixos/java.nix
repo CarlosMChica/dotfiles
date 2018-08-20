d052dbd98b7a315fc7a03e9d9952b0dfe7d99c0e
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-20 21:45:30 +0100

    Add basic setup for mango shop

--- nixos/java.nix
+++ nixos/java.nix
@@ -1,11 +1,32 @@
 { config, pkgs, ... }:
 
+let
+  tomcat7-0-42 = pkgs.stdenv.lib.overrideDerivation pkgs.tomcat7 (oldAttrs : {
+    name = "apache-tomcat-7.0.42";
+    version = "7.0.42";
+    src = pkgs.fetchurl {
+      url = "https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.42/bin/apache-tomcat-7.0.42.tar.gz";
+      sha256 = "1prn3jcxvykddl4b430jn3zvflssw3k9d38dricw43qqsxigfqy1";
+    };
+});
+in
 {
+  #mangoshop
+  networking.hosts = {
+    "127.0.0.1" = [ "localhost.mango.com" "litedb"];
+  };
+
+  programs.java = {
+    enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
-    openjdk
-#    openjdk10
     maven
     gradle
     jetbrains.idea-ultimate
+
+    #mangoshop
+    subversion
+    tomcat7-0-42
   ];
 }
