73ce98f599b4d1b379312eb301e61953d3d69be6
Carlos Morera de la Chica <carlos@codurance.com>
2018-08-22 08:52:58 +0100

    system/screen.sh
    Add some screen config scripts for lenovox230

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -5,8 +5,16 @@
   networking = {
     networkmanager.enable = true;
   };
-
   networking.firewall.enable = false;
+
+  services.openvpn.servers = {
+    coduranceVPN  = { config = '' config /home/carlos/Downloads/34.242.136.162-direct.ovpn ''; };
+    haskellVPN = {
+      config = '' config /home/carlos/Downloads/188.166.3.148-direct.ovpn '';
+      autoStart = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     iw
     networkmanagerapplet
--- system/screen.sh
+++ system/screen.sh
@@ -43,3 +43,39 @@ screenMacbookLeftLayout() {
     --output HDMI1 --off
   variety --next
 }
+
+screenLenovoX230Layout() {
+  xrandr --output LVDS1 --mode 1366x768 --pos 2560x312 --rotate normal --output VGA1 --off \
+         --output eDP1 --off \
+         --output VIRTUAL1 --off \
+         --output DP1 --off \
+         --output HDMI2 --off \
+         --output HDMI1 --off \
+         --output DP2 --off
+  variety --next
+}
+
+screenLenovoX230RightLayout() {
+  xrandr --output DP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal \
+         --output LVDS1 --mode 1366x768 --pos 2560x312 --rotate normal --output VGA1 --off \
+         --output VIRTUAL1 --off \
+         --output DP3 --off \
+         --output DP2 --off \
+         --output HDMI3 --off \
+         --output HDMI2 --off \
+         --output HDMI1 --off \
+  variety --next
+}
+
+screenLenovoX230LeftLayout() {
+  xrandr --output DP1 --primary --mode 2560x1440 --pos 1366x0 --rotate normal \
+         --output LVDS1 --mode 1366x768 --pos 0x376 --rotate normal \
+         --output DP3 --off \
+         --output DP2 --off \
+         --output HDMI3 --off \
+         --output HDMI2 --off \
+         --output VIRTUAL1 --off \
+         --output HDMI1 --off \
+         --output VGA1 --off
+  variety --next
+}
