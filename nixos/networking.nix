e40c45cdbdf40ddb5b94cc1e40582c2d81b20032
Carlos Morera de la Chica <carlos@codurance.com>
2018-09-13 07:34:47 +0100

    Disable VPNs by default

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -8,10 +8,13 @@
   networking.firewall.enable = false;
 
   services.openvpn.servers = {
-    coduranceVPN  = { config = '' config /home/carlos/Downloads/34.242.136.162-direct.ovpn ''; };
+    coduranceVPN = {
+      config = '' config /home/carlos/Downloads/34.242.136.162-direct.ovpn '';
+      autoStart = false;
+    };
     haskellVPN = {
       config = '' config /home/carlos/Downloads/188.166.3.148-direct.ovpn '';
-      autoStart = true;
+      autoStart = false;
     };
   };
 
