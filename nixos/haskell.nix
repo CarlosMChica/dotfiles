13bbcf528784773365a0468ca1dc515ad9e3ffd4
Carlos Morera de la Chica <carlos@codurance.com>
2018-11-12 10:29:56 +0000

    Remove cachix as it is not used for now

--- nixos/haskell.nix
+++ nixos/haskell.nix
@@ -1,17 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  nix = {
-    binaryCaches = [
-      "https://cache.nixos.org/"
-      "https://hie-nix.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY="
-    ];
-    trustedUsers = [ "root" "carlos" ];
-  };
-
   environment.systemPackages = with pkgs; [
     haskell.compiler.ghc862
     stack
@@ -21,7 +10,6 @@
     haskellPackages.hlint
     haskellPackages.hoogle
     haskellPackages.cabal2nix
-    cachix
   ];
 
 }
