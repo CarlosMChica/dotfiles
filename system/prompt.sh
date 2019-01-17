4e44b483fbd9e15b590ccb69f7872a1e0885604c
Carlos Morera de la Chica <carlos.delachica@habito.com>
2019-01-17 11:29:49 +0000

    Completely delete old powerline config

--- system/prompt.sh
+++ system/prompt.sh
@@ -2,14 +2,6 @@
 
 export PROMPT_DIRTRIM=2
 
-#powerline-daemon -q
-#POWERLINE_COMMAND=$(which powerline-hs)
-#POWERLINE_CONFIG_COMMAND=$(which true)
-#POWERLINE_BASH_CONTINUATION=1
-#POWERLINE_BASH_SELECT=1
-#POWERLINE_PATH=$(readlink -f "$(which powerline)")
-#. "${POWERLINE_PATH%bin/*}/lib/python3.6/site-packages/powerline/bindings/bash/powerline.sh"
-
 function _update_ps1() {
   POWERLINE_GO=$(which powerline-go)
   PS1="$($POWERLINE_GO  \
