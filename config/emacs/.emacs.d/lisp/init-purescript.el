898014940da8971d3849de14a2e8cf9cc27b297a
Carlos Morera de la Chica <carlos.delachica@habito.com>
2019-01-17 11:30:03 +0000

    Tweak purescript emacs integration

--- config/emacs/.emacs.d/lisp/init-purescript.el
+++ config/emacs/.emacs.d/lisp/init-purescript.el
@@ -13,17 +13,21 @@
               (turn-on-purescript-indentation)))
   (add-hook 'purescript-mode-hook 'programming-mode))
 
-;(use-package psc-ide
-;  :diminish psc-ide-mode
-;  :ensure t
-;  :defer t
-;  :config
-;  (setq psc-ide-use-npm-bin t)
+(define-key evil-normal-state-map (kbd "M-.")
+  `(menu-item "" evil-repeat-pop :filter
+              ,(lambda (cmd) (if (eq last-command 'evil-repeat-pop) cmd))))
+
+(use-package psc-ide
+  :diminish psc-ide-mode
+  :ensure t
+  :defer t
+  :config
+  (setq psc-ide-use-npm-bin t)
 ;  (setq psc-ide-output-directory "build/")
-;  (setq browse-url-browser-function 'eww-browse-url)
-;  (customize-set-variable 'psc-ide-rebuild-on-save t))
-;
-;(use-package psci :ensure t :defer t)
-;(use-package flycheck-purescript :ensure t :defer t)
+  (setq browse-url-browser-function 'eww-browse-url)
+  (customize-set-variable 'psc-ide-rebuild-on-save t))
+
+(use-package psci :ensure t :defer t)
+(use-package flycheck-purescript :ensure t :defer t)
 
 (provide 'init-purescript)
