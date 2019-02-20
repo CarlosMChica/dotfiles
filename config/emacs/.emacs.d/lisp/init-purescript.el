4004852d31c51fd3a21104c1a1cc3eb298dc5d63
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2019-02-20 11:53:47 +0000

    Revert "Leave barebone purescript setup"
    This reverts commit 3eae02075333db2a04ba2492746b3f5f5a9bd7b4.

--- config/emacs/.emacs.d/lisp/init-purescript.el
+++ config/emacs/.emacs.d/lisp/init-purescript.el
@@ -5,6 +5,30 @@
   :ensure t
   :defer t
   :config
+  (setq projectile-tags-command "npm run etags")
+  (add-hook 'purescript-mode-hook
+            (lambda ()
+              (psc-ide-mode)
+              (company-mode)
+              (flycheck-mode)
+              (turn-on-purescript-indentation)))
   (add-hook 'purescript-mode-hook 'programming-mode))
+
+(define-key evil-normal-state-map (kbd "M-.")
+  `(menu-item "" evil-repeat-pop :filter
+              ,(lambda (cmd) (if (eq last-command 'evil-repeat-pop) cmd))))
+
+(use-package psc-ide
+  :diminish psc-ide-mode
+  :ensure t
+  :defer t
+  :config
+  (setq psc-ide-use-npm-bin t)
+;  (setq psc-ide-output-directory "build/")
+  (setq browse-url-browser-function 'eww-browse-url)
+  (customize-set-variable 'psc-ide-rebuild-on-save t))
+
+(use-package psci :ensure t :defer t)
+(use-package flycheck-purescript :ensure t :defer t)
 
 (provide 'init-purescript)
