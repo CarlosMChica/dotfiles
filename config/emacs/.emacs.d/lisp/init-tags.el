7432dd931a7deac02401fae9a7cc4a1894699642
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2019-03-09 16:19:03 +0000

    Enforce xref shortcuts over evil

--- config/emacs/.emacs.d/lisp/init-tags.el
+++ config/emacs/.emacs.d/lisp/init-tags.el
@@ -10,6 +10,13 @@
   (set-face-foreground 'helm-xref-file-name "#81A1C1")
   (setq helm-xref-candidate-formatting-function  'helm-xref-format-candidate-long))
 
+(global-set-key (kbd "M-.") 'xref-find-definitions)
+(define-key evil-normal-state-map (kbd "M-.") 'xref-find-definitions)
+(global-set-key (kbd "M-?") 'xref-find-references)
+(define-key evil-normal-state-map (kbd "M-?") 'xref-find-references)
+(global-set-key (kbd "M-,") 'xref-pop-marker-stack)
+(define-key evil-normal-state-map (kbd "M-,") 'xref-pop-marker-stack)
+
 (location-list-buffer (rx bos "*helm-xref*"))
 (location-list-buffer (rx bos "*helm-mode-xref*"))
 (location-list-buffer (rx bos "*xref*"))
