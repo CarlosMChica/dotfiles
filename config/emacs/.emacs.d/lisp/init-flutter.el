ad8a6e635f84e2f79fba6eb96596ab6592d9ebe9
Christian Panadero <panavtec@gmail.com>
2019-05-27 16:45:12 +0100

    flutter support for emacs

---
+++ config/emacs/.emacs.d/lisp/init-flutter.el
@@ -0,0 +1,39 @@
+(require 'init-programming-mode)
+
+;(use-package
+;  eglot
+;  :ensure t)
+
+(use-package
+  lsp-mode
+  :ensure t
+  :config
+  (setq lsp-enable-snippet nil))
+
+(use-package
+  dart-mode
+  :ensure t
+  :mode "\\.dart$"
+  :config
+    (add-hook 'dart-mode-hook 'programming-mode)
+    (add-hook 'dart-mode-hook 'lsp)
+    (with-eval-after-load "projectile"
+      (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+      (add-to-list 'projectile-project-root-files-bottom-up "BUILD"))
+    (setq lsp-auto-guess-root t)
+;    (with-eval-after-load "eglot"
+;      (add-to-list 'eglot-server-programs '(dart-mode . ("dart_language_server")))
+;      (add-hook 'dart-mode-hook 'eglot-ensure))
+    (setq dart-format-on-save t)
+    (setq dart-sdk-path "~/flutter/bin/cache/dart-sdk/"))
+
+(use-package
+  flutter
+  :ensure t
+  :after dart-mode
+  :bind (:map dart-mode-map
+              ("C-M-x" . #'flutter-run-or-hot-reload))
+  :config
+    (setq flutter-sdk-path "~/flutter/bin/cache/dart-sdk/"))
+
+(provide 'init-flutter)
