16175435a0ef9dbbc0b82f5219b0cc9d1415c7e7
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2019-11-21 14:38:19 +0000

    Revert "Disable tabnine"
    This reverts commit 81970b69d226743f51475789034053c6bedb6068.

--- config/emacs/.emacs.d/lisp/init-auto-complete.el
+++ config/emacs/.emacs.d/lisp/init-auto-complete.el
@@ -6,10 +6,10 @@
   (setq company-idle-delay 0.3)
   (setq company-show-numbers t))
 
-;; (use-package
-;;   company-tabnine
-;;   :ensure t
-;;   :after company
-;;   :config (add-to-list 'company-backends #'company-tabnine))
+(use-package
+  company-tabnine
+  :ensure t
+  :after company
+  :config (add-to-list 'company-backends #'company-tabnine))
 
 (provide 'init-auto-complete)
