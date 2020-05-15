178479eefab5348cfe71379f568f6d94bd0c45a5
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2020-05-15 09:37:46 +0100

    Add steam to nixos

--- nixos/apps.nix
+++ nixos/apps.nix
@@ -12,5 +12,11 @@
     vlc
     baobab
     gnome3.file-roller
+    steam
   ];
+
+  hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932
+    enable = true;
+    driSupport32Bit = true;
+  };
 }
