dd1cbb523f75c9e633c720346590ac601f11238e
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2020-06-01 23:12:10 +0100

    Add slack to nixos

--- nixos/apps.nix
+++ nixos/apps.nix
@@ -13,6 +13,7 @@
     baobab
     gnome3.file-roller
     steam
+    slack
   ];
 
   hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932
