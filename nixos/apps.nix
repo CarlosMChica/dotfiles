55f7e7d54d26dbb024c7c9159de38fd1453d5eac
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2020-08-25 14:03:30 +0100

    Remove steam

--- nixos/apps.nix
+++ nixos/apps.nix
@@ -11,12 +11,7 @@
     vlc
     baobab
     gnome3.file-roller
-    steam
     slack
   ];
 
-  hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932
-    enable = true;
-    driSupport32Bit = true;
-  };
 }
