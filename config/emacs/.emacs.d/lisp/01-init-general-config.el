aead47252fd358af50a027f49b6524dd03ae6350
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2020-09-17 13:19:59 +0100

    Add emacs auto package update

--- config/emacs/.emacs.d/lisp/01-init-general-config.el
+++ config/emacs/.emacs.d/lisp/01-init-general-config.el
@@ -83,4 +83,13 @@
     (add-hook 'post-command-hook 'set-terminal-title)
     (add-hook 'post-command-hook 'set-window-title))
 
+;; Auto update packages
+(use-package auto-package-update
+   :ensure t
+   :config
+   (setq auto-package-update-delete-old-versions t
+         auto-package-update-interval 1
+         auto-package-update-prompt-before-update t)
+   (auto-package-update-maybe))
+
 (provide '01-init-general-config)
